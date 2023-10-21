778418fe0dce612dbdea27fb61cdc87adf6e5d52
ScSmlik <2738030477@qq.com>
2023-10-21 15:37:18 +0800

    LWHChange1

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -2,13 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class test : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
-    private float tt;
+    // Start is called before the first frame update
     void Start()
     {
-        tt = 2.5f;
-        Debug.Log("Test");
+        
     }
 
     // Update is called once per frame
